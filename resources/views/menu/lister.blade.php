712ba816ef98f8947a84b463bedccdea1e45d9a5
lichenglonaa$ <34432269+lichenglon@users.noreply.github.com>
2018-03-28 15:25:30 +0800

    menu-lister

--- resources/views/menu/lister.blade.php
+++ resources/views/menu/lister.blade.php
@@ -6,13 +6,8 @@
         <meta name="description" content="">
         <meta name="author" content="">
         <link rel="shortcut icon" href="{{ asset('images') }}/favicon.png" type="image/png">
-
-        <title>Ulzz.com</title>
-        @include('tpl.CommonJs')
-
         <title>Ulzz.com</title>  
         @include('tpl.CommonJs')      
-
     </head>
     <body>
         <!-- Preloader -->
@@ -31,105 +26,7 @@
                         <button class="btn btn-success btn-add" set-lan="html:ADDMENU">添加菜单</button>
                     </div>
                     <div class="menu-content">
-
-                        <div class="menu-list">
-                            <div class="menu-list-item menu-list-header">
-                                <div class="menu-list-parent">
-                                    <div class="fold" set-lan="html:FOLD">折叠</div>
-                                    <div class="order">ID</div>
-                                    <div class="order" set-lan="html:SORT">排序</div>
-                                    <div class="width-lg" set-lan="html:NAME">名称</div>
-                                    <div class="width-lg">ICON</div>
-                                    <div class="width-lg">URL</div>
-                                    <div class="fold"></div>
-                                </div>
-                            </div>
-                            <div class="menu-list-item">
-                                <div class="menu-list-parent">
-                                    <div class="fold"></div>
-                                    <div class="order">1</div>
-                                    <div class="order"><input type="text" class="form-control text-center" value="0"></div>
-                                    <div class="width-lg"><input type="text" class="form-control" value="首页"></div>
-                                    <div class="width-lg"><input type="text" class="form-control" value="fa-home"></div>
-                                    <div class="width-lg"><input type="text" class="form-control" value="/"></div>
-                                    <div class="fold"><i class="fa fa-plus-circle fa-big"></i></div>
-                                    <div class="fold pull-right">
-                                        <span set-lan="html:DELETE">删除</span>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="menu-list-item">
-                                <div class="menu-list-parent">
-                                    <div class="fold"><i class="fa fa-plus-square-o icon-btn"></i></div>
-                                    <div class="order">3</div>
-                                    <div class="order"><input type="text" class="form-control text-center" value="1"></div>
-                                    <div class="width-lg"><input type="text" class="form-control" value="账号管理"></div>
-                                    <div class="width-lg"><input type="text" class="form-control" value="fa-laptop"></div>
-                                    <div class="width-lg"><input type="text" class="form-control" value=""></div>
-                                    <div class="fold"><i class="fa fa-plus-circle fa-big"></i></div>
-                                    <div class="fold pull-right">
-                                        <span set-lan="html:DELETE">删除</span>
-                                    </div>
-                                </div>
-                                <div class="menu-list-child">
-                                    <div class="menu-list-parent">
-                                        <div class="fold"></div>
-                                        <div class="order">4</div>
-                                        <div class="order"><input type="text" class="form-control text-center" value="0"></div>
-                                        <div class="fold fold-line"></div>
-                                        <div class="width-lg"><input type="text" class="form-control" value="账号管理"></div>
-                                        <div class="width-lg"><input type="text" class="form-control" value="/account/user"></div>
-                                        <div class="fold pull-right">
-                                            <span set-lan="html:DELETE">删除</span>
-                                        </div>
-                                    </div>
-                                    <div class="menu-list-parent">
-                                        <div class="fold"></div>
-                                        <div class="order">5</div>
-                                        <div class="order"><input type="text" class="form-control text-center" value="1"></div>
-                                        <div class="fold fold-line"></div>
-                                        <div class="width-lg"><input type="text" class="form-control" value="角色列表"></div>
-                                        <div class="width-lg"><input type="text" class="form-control" value="/account/role"></div>
-                                        <div class="fold pull-right">
-                                            <span set-lan="html:DELETE">删除</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="menu-list-item">
-                                <div class="menu-list-parent">
-                                    <div class="fold"><i class="fa fa-plus-square-o icon-btn"></i></div>
-                                    <div class="order">3</div>
-                                    <div class="order"><input type="text" class="form-control text-center" value="1"></div>
-                                    <div class="width-lg"><input type="text" class="form-control" value="菜单管理"></div>
-                                    <div class="width-lg"><input type="text" class="form-control" value="fa-reorder"></div>
-                                    <div class="width-lg"><input type="text" class="form-control" value=""></div>
-                                    <div class="fold"><i class="fa fa-plus-circle fa-big"></i></div>
-                                    <div class="fold pull-right">
-                                        <span set-lan="html:DELETE">删除</span>
-                                    </div>
-                                </div>
-                                <div class="menu-list-child">
-                                    <div class="menu-list-parent">
-                                        <div class="fold"></div>
-                                        <div class="order">4</div>
-                                        <div class="order"><input type="text" class="form-control text-center" value="0"></div>
-                                        <div class="fold fold-line"></div>
-                                        <div class="width-lg"><input type="text" class="form-control" value="菜单列表"></div>
-                                        <div class="width-lg"><input type="text" class="form-control" value="/account/user"></div>
-                                        <div class="fold pull-right">
-                                            <span set-lan="html:DELETE">删除</span>
-                                        </div>
-
-                                    </div>
-
-                                    </div>                                    
-
-                                </div>
-                            </div>
-
                         <div class="menu-list" id="MenuList">
-
                         </div>
                     </div>
                 </div><!-- contentpanel -->
@@ -273,11 +170,7 @@
                                 <div class="col-sm-8">
                                     <input type="text" placeholder="" class="form-control">
                                 </div>
-
-                            </div>
-
                             </div>                
-
                         </form>
                         <div class="add_btn">
                             <span id="AddMeanuChildSave" set-lan="html:Save">保存</span>
@@ -286,90 +179,6 @@
                 </div>
             </div>
         </div>
-
-
-    </div>
-</div>
-</body>
-<script>
-    $(document).on('click','.fold>.icon-btn',function () {
-        var child=$(this).parents(".menu-list-item").find(".menu-list-child");
-        if(child.is(":hidden")){
-            child.slideDown('fast');
-            $(this).removeClass("fa-plus-square-o");
-            $(this).addClass("fa-minus-square-o");
-        }else{
-            child.slideUp('fast');
-            $(this).removeClass("fa-minus-square-o");
-            $(this).addClass("fa-plus-square-o");
-        }
-    });
-    $(".btn-fold").click(function(){
-        $(".menu-list-child").hide();
-    });
-    $(".btn-open").click(function(){
-        $(".menu-list-child").show();
-    });
-    $(".btn-add").click(function(){
-        $("#AddMenuModal").modal();
-    });
-    $(document).on('click','.fa-big',function(){
-        $("#AddMenuChildModal").modal();
-    });
-</script>
-<script src="{{ asset('js') }}/language.js"></script>
-
-    </body>
-    <script>
-        $(document).on('click','.fold>.icon-btn',function () {
-            var child=$(this).parents(".menu-list-item").find(".menu-list-child");
-            if(child.is(":hidden")){
-                child.slideDown('fast');
-                $(this).removeClass("fa-plus-square-o");
-                $(this).addClass("fa-minus-square-o");
-            }else{
-                child.slideUp('fast');
-                $(this).removeClass("fa-minus-square-o");
-                $(this).addClass("fa-plus-square-o");
-            }
-        });
-        $(".btn-fold").click(function(){
-            $(".menu-list-child").hide();
-            $(".fold>.icon-btn").removeClass("fa-minus-square-o");
-            $(".fold>.icon-btn").addClass("fa-plus-square-o");
-        });
-        $(".btn-open").click(function(){
-            $(".menu-list-child").show();
-            $(".fold>.icon-btn").removeClass("fa-plus-square-o");
-            $(".fold>.icon-btn").addClass("fa-minus-square-o");
-        });
-        $(".btn-add").click(function(){
-            $("#AddMenuModal").modal();
-        });
-        $(document).on('click','.fa-big',function(){
-            $("#AddMenuChildModal").modal();
-        });
-        function MenuList(){
-            $.ajax({
-                url:"{{url('menu/lister')}}",
-                type:'POST', //GET
-                async:false,    //或false,是否异步
-                timeout:5000,    //超时时间
-                data:{"_token":"{{csrf_token()}}"},
-                dataType:'json',    //返回的数据格式：json/xml/html/script/jsonp/text
-                success:function(res){
-                    console.log(res);
-                    if(res.status=="1"){
-
-                    }
-                },
-                error:function(){
-                    console.log('错误');
-                }
-            });
-        }
-        MenuList();
-
     </body>
     <script type="text/html" id="MenuHtml">
         <div class="menu-list-item menu-list-header">
@@ -401,7 +210,7 @@
         </div>
         @{{else}}
         <div class="menu-list-item">
-            <div class="menu-list-parent">
+            <div class="menu-list-parent" ItemId="@{{$value.id}}">
                 <div class="fold"><i class="fa fa-plus-square-o icon-btn"></i></div>
                 <div class="order">@{{$value.id}}</div>
                 <div class="order"><input type="text" class="form-control text-center" value="@{{$value.list_order}}"></div>
@@ -415,7 +224,7 @@
             </div>
             <div class="menu-list-child">
                 @{{each $value.child as child index}}
-                <div class="menu-list-parent">
+                <div class="menu-list-parent" ItemId="@{{$value.id}}">
                     <div class="fold"></div>
                     <div class="order">@{{child.id}}</div>
                     <div class="order"><input type="text" class="form-control text-center" value="@{{child.id}}"></div>
@@ -481,12 +290,7 @@
                 }
             });  
         }
-
-        //MenuList();
-
-
         MenuList();
-
     </script>
     <script src="{{ asset('js') }}/language.js"></script>
 </html>
